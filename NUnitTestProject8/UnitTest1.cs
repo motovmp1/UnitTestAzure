fa38cebd4ada446c9423144ae91d6c92b870f028
motovmp1 <vinicius.mpinho@gmail.com>
2020-11-18 10:57:33 +0000

    first commit

---
+++ NUnitTestProject8/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace NUnitTestProject8
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            System.Console.WriteLine("first step");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int num = 1;
+            System.Console.WriteLine("second step inside");
+
+            Assert.That(num, Is.EqualTo(1));
+        }
+    }
+}
